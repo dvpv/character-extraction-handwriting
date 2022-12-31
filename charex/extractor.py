4da086cb7f8475a83dbe3462502945c30f0e86de
dvpv <david.pava@protonmail.com>
2022-12-31 17:16:16 +0200

    line extraction

--- charex/extractor.py
+++ charex/extractor.py
@@ -1,5 +1,82 @@
 import numpy as np
+import cv2
+from . import output
+from .utils.colorcycle import ColorCycle
+from typing import List
+
+DEFAULT_LINE_CONTOUR_THRESH = 200
+DEFAULT_WIDTH_DILATE_MULTIPLIER = 0.1
+DEFAULT_BLUR_SIGMA = 0.7
+DEFAULT_MASK_COLOR = (0xFF, 0xFF, 0xFF)  # #FFF
 
 
 def extract(image_path: str) -> np.array:
     raise NotImplemented
+
+
+def extract_rows(image: np.array, preview_flag: bool = False) -> List[np.array]:
+    # Save an image copy
+    original = np.copy(image)
+
+    # Grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Blur out noise
+    image = cv2.GaussianBlur(image, (0, 0), DEFAULT_BLUR_SIGMA)
+    # Grayscale to binary
+    image = cv2.adaptiveThreshold(
+        image,
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY_INV,
+        blockSize=11,
+        C=5,
+    )
+
+    # Dilate image horizontally
+    _, width, _ = original.shape
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT,
+        ksize=(int(width * DEFAULT_WIDTH_DILATE_MULTIPLIER), 1),
+    )
+    dilated = cv2.dilate(image, kernel, iterations=1)
+    # Get contours for each line
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Extract lines
+    lines: List[np.array] = []
+    for contour in contours:
+        if cv2.contourArea(contour) > DEFAULT_LINE_CONTOUR_THRESH:
+            (x, y, w, h) = cv2.boundingRect(contour)
+            line = np.copy(original)
+            # Build the mask
+            mask = np.zeros(original.shape[:2], np.uint8)
+            cv2.drawContours(mask, [contour], -1, DEFAULT_MASK_COLOR, -1, cv2.LINE_AA)
+            # Crop line
+            line = cv2.bitwise_and(original, original, mask=mask)
+            # Apply white background
+            background = np.ones(original.shape, np.uint8) * 255
+            cv2.bitwise_not(background, background, mask=mask)
+            line += background
+            line = line[y : y + h, x : x + w]
+            lines.append(line)
+
+    # Show a nicely formatted image representing the lines with the lines
+    if preview_flag:
+        cc = ColorCycle()
+        rectangles = np.copy(original)
+
+        for contour in contours:
+            if cv2.contourArea(contour) > DEFAULT_LINE_CONTOUR_THRESH:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(
+                    img=rectangles,
+                    pt1=(x, y),
+                    pt2=(x + w, y + h),
+                    color=cc.next(),
+                    thickness=-1,
+                )
+        overlay = np.copy(original)
+        overlay = cv2.addWeighted(overlay, 0.6, rectangles, 0.5, 0)
+        output.preview(overlay)
+
+    return lines
--- charex/processor.py
+++ charex/processor.py
@@ -1,70 +1,15 @@
 from . import extractor, output, reader
 from typing import Optional
 import cv2
-import numpy as np
-import math
 
 
 def process(input_file: str, output_file: Optional[str], preview_flag: bool) -> None:
     original = reader.read(input_file)
     resized = cv2.resize(original, [1000, 1000])
-    image = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
-    output.preview(image)
-    image = cv2.GaussianBlur(image, (0, 0), 0.7)
-    output.preview(image)
-    image = cv2.adaptiveThreshold(
-        image,
-        255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY_INV,
-        21,
-        10,
-    )
-    output.preview(image)
-    kernel = np.ones((5, 5))
-    dilated = cv2.dilate(image, kernel, iterations=1)
-    output.preview(dilated)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    cropped = resized[10:-10, 10:-10]
 
-    ## Split big horizontal contours
+    rows = extractor.extract_rows(cropped, preview_flag=preview_flag)
 
-    # Calculate average contour size
-    avr_contour_size = sum(cv2.boundingRect(c)[3] for c in contours) / len(contours)
-    print(f"Average h size = {avr_contour_size}")
-
-    # Split biggest blobs
-
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-        if h * 30 / 100 > avr_contour_size:
-            print(dilated[x, y])
-            dilated[int(y + h / 2) : int(y + h / 2 + 5), x : x + w] = 0
-    output.preview(dilated)
-
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(
-            img=resized,
-            pt1=(x, y),
-            pt2=(x + w, y + h),
-            color=(0, 255, 0),
-            thickness=2,
-        )
-    output.preview(resized)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-        cv2.rectangle(
-            img=resized,
-            pt1=(x, y),
-            pt2=(x + w, y + h),
-            color=(0, 255, 0),
-            thickness=2,
-        )
-    output.preview(resized)
-
-    edges = cv2.Canny(image, 50, 200, None, 3)
-    output.preview(edges)
+    for row in rows:
+        output.preview(row)
     # WIP
