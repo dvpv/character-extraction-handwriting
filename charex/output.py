418edbb13131caefcb17235b6d736c272a554f17
dvpv <david.pava@protonmail.com>
2023-01-02 14:02:37 +0200

    added scalable output

--- charex/output.py
+++ charex/output.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2
 
+DEFAULT_PREVIEW_WIDTH = 1000
+
 
 def preview(image: np.array, title: str = "Preview") -> None:
-    cv2.imshow(title, image)
+    width, height, _ = image.shape
+    resized = cv2.resize(
+        image,
+        [DEFAULT_PREVIEW_WIDTH, int(width * DEFAULT_PREVIEW_WIDTH / height)],
+    )
+    cv2.imshow(title, resized)
     cv2.waitKey(0)
